69ebfa7f287f64e55fa99ce5ed828f348252e83c
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-06-01 01:17:35 +0530

    Drawer create form

--- app/controllers/drawers_controller.rb
+++ app/controllers/drawers_controller.rb
@@ -1,5 +1,14 @@
 class DrawersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @drawers = current_user.drawers
+    @drawer = Drawer.new
+  end
+
+  def create
+    Drawer.create!(name: params[:drawer][:name], user: current_user)
+
+    redirect_to :drawers
   end
 end
